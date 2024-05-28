383cafe125a7263a5d5ec22025a587ca917969cc
Lisandro Luis Knott <lisandrok@gmail.com>
2024-05-28 22:43:10 +0200

    Mejoras en las clases Inmueble e Incidencia

--- class/Incidencia.php
+++ class/Incidencia.php
@@ -6,19 +6,26 @@ class Incidencia {
     private $titulo;
     private $descripcion;
     private $estado;
-    private $usuario; //Aqui debe guardarse un objeto usuario
-    private $inmueble; //Aqui debe guardarse un inmueble
     private $fecha_y_hora;
 
-    public function __construct ($id, $tipo, $titulo, $descripcion, $estado, $usuario, $inmueble, $fecha_y_hora) {
+    public function __construct ($id, $tipo, $titulo, $descripcion, $estado, $fecha_y_hora) {
         $this->id = $id;
         $this->$tipo = $tipo;
         $this->titulo = $titulo;
         $this->descripcion = $descripcion;
         $this->estado = $estado;
-        $this->usuario = $usuario;
-        $this->inmueble = $inmueble;
         $this->fecha_y_hora = $fecha_y_hora;
+    }
+
+    public static function obtenerIncidenciaExistente($id){
+        $consulta = Conexion::consulta("SELECT id, tipo, titulo, descripcion, estado, fecha_y_hora FROM incidencias WHERE inmueble_id=" . $id);
+
+        foreach ($consulta as $fila) {
+            $incidencia = new Incidencia($fila['id'], $fila['tipo'], $fila['titulo'], $fila['descripcion'], $fila['estado'], $fila['fecha_y_hora']);
+        }
+
+        return $incidencia;
+
     }
 
 
--- class/Inmueble.php
+++ class/Inmueble.php
@@ -5,18 +5,30 @@ class Inmueble {
     private $id;
     private $direccion;
     private $inquilino;
+    private $incidencias;
 
-    public function __construct($id, $direccion, $inquilino) {
+    public function __construct($id, $direccion, $inquilino, $incidencias) {
         $this->id = $id;
         $this->direccion = $direccion;
         $this->inquilino = $inquilino;
+        $this->incidencias = $incidencias;
     }
 
     public static function obtenerInmuebleExistente($id) {
-        $consulta = Conexion::consulta("SELECT id, direccion, propietario_usuario_id, inquilino_usuario_id FROM inmuebles WHERE id=" . $id);
+        //Primero obtengo las posibles incidencias del inmueble ya que las voy a necesitar para instanciarlo
+        $consulta = Conexion::consulta("SELECT id, tipo, titulo, descripcion, estado, usuario_id, fecha_y_hora FROM incidencias WHERE inmueble_id=" . $id);
+
+        foreach($consulta as $fila) {
+            $incidencia = Incidencia::obtenerIncidenciaExistente($fila["id"]);
+            $incidencias[] = $incidencia;
+        }
+
+        
+        //Obtengo los datos del inmueble desde la db
+        $consulta = Conexion::consulta("SELECT id, direccion, inquilino_usuario_id FROM inmuebles WHERE id=" . $id);
 
         foreach ($consulta as $fila) {
-            $inmueble = new Inmueble($fila['id'], $fila['direccion'], Usuario::obtenerUsuarioExistente($fila['inquilino_usuario_id']));
+            $inmueble = new Inmueble($fila['id'], $fila['direccion'], Usuario::obtenerUsuarioExistente($fila['inquilino_usuario_id']), $incidencias);
         }
 
         return $inmueble;
