22775a86d2daef971c936df4b0bb77bbef342c83
Lisandro Luis Knott <lisandrok@gmail.com>
2024-06-08 20:57:55 +0200

    Cambios en la navbar para que funcione correctamente en dispositivos moviles

--- include/nav.php
+++ include/nav.php
@@ -1,24 +1,26 @@
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="./">
-                    <i class="bi bi-house-heart-fill"></i> Alquiler +
-                </a>
-                <div class="collapse navbar-collapse">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                        <?php if (isset($_SESSION['email'])) {
-                ?>
-                            <a class="nav-link btn btn-outline-danger" href="logout.php">
-                                <i class="bi bi-box-arrow-right"></i> Cerrar sesi&oacute;n
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="./">
+                <i class="bi bi-house-heart-fill"></i> Alquiler +
+            </a>
+            <div class="ml-auto">
                 <?php
-            } else {
+                    if (isset($_SESSION['email'])) {
+                        ?>
+
+                    <a class="nav-link btn btn-outline-danger" href="logout.php">
+                        <i class="bi bi-box-arrow-right"></i> Cerrar sesi&oacute;n</a>
+
+                        <?php
+                    } else {
+                        ?>
+
+                <a class="nav-link btn btn-outline-primary" href="login.php">
+                    <i class="bi bi-box-arrow-in-right"></i> Ingresar</a>
+
+                        <?php
+                    }
                 ?>
-                            <a class="nav-link btn btn-outline-primary" href="login.php">
-                                <i class="bi bi-box-arrow-in-right"></i> Ingresar
-            <?php }?>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
             </div>
-        </nav>
+        </div>
+    </nav>
