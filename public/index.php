be35ef728247f8fc0d70ba2feac62410d826d456
Lisandro Luis Knott <lisandrok@gmail.com>
2024-06-10 12:35:09 +0200

    Ajustes menores para que se vea el texto del carrusel en dispositivos moviles. Se recortan imagenes y se elimina la de construccion dado que no se deja ninguna pagina en construccion.

--- public/index.php
+++ public/index.php
@@ -32,22 +32,22 @@ if (isset($_SESSION['email'])) {
             <div id="serviceCarousel" class="carousel slide" data-ride="carousel" data-interval="5000">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="img/imagen1.jpg" class="d-block w-100" alt="Service 1">
-                        <div class="carousel-caption d-none d-md-block">
+                        <img src="img/imagen1.jpg" class="d-block w-100" alt="Sistema de gesti&oacute;n de alquileres para peque&ntilde;os propietarios">
+                        <div class="carousel-caption d-md-block">
                             <h5>Gestiona tus alquileres</h5>
                             <p>Sistema de gesti&oacute;n de alquileres para peque&ntilde;os propietarios</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="img/imagen2.jpg" class="d-block w-100" alt="Service 2">
-                        <div class="carousel-caption d-none d-md-block">
+                        <img src="img/imagen2.jpg" class="d-block w-100" alt="Comienza a utilizar la versi&oacute;n gratuita de Alquiler +">
+                        <div class="carousel-caption d-md-block">
                             <h5>Econ&oacute;mico</h5>
                             <p>Comienza a utilizar la versi&oacute;n gratuita de Alquiler +</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="img/imagen3.jpg" class="d-block w-100" alt="Service 3">
-                        <div class="carousel-caption d-none d-md-block">
+                        <img src="img/imagen3.jpg" class="d-block w-100" alt="Gestiona los incidentes que afectan tu inmueble de manera sencilla">
+                        <div class="carousel-caption d-md-block">
                             <h5>Gesti&oacute;n de incidentes</h5>
                             <p>Gestiona los incidentes que afectan tu inmueble de manera sencilla</p>
                         </div>
@@ -55,11 +55,11 @@ if (isset($_SESSION['email'])) {
                 </div>
                 <a class="carousel-control-prev" href="#serviceCarousel" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
+                    <span class="sr-only">Previa</span>
                 </a>
                 <a class="carousel-control-next" href="#serviceCarousel" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
+                    <span class="sr-only">Siguiente</span>
                 </a>
             </div>
         </div>
